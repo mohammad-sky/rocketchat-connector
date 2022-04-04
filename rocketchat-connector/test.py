1df277089261bb7b06e86653115cab0e8a324f70
m.sepehr <83414472+sepehrmizani@users.noreply.github.com>
2022-04-04 20:51:53 +0430

    Update test.py

--- rocketchat-connector/test.py
+++ rocketchat-connector/test.py
@@ -2,4 +2,8 @@ from __init__ import  rocket_connector
 import time
 messenger = rocket_connector()
 messenger.find_username("neme")
+
 messenger.send_message("text")
+
+messenger.change_status("away")
+
